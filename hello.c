84c992c5d833a4bc6cfd67a35e2bf063c5c52745
Gaurav Joshi <gj451026@gmail.com>
2023-05-19 18:11:27 +0530

    Hello World! printed

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+    printf("\n Enter the values for a: ");
+    scanf("%d", &a);
+    printf("\n Enter the values for b: ");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("\nThe sum is %d\n", sum);
+}
